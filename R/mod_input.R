c5eff518671d35d8ec67c0956e41ae336a34cfaf
bluefoxr <william.becker@bluefoxdata.eu>
2023-05-12 14:05:44 +0200

    Add country dropdown

--- R/mod_input.R
+++ R/mod_input.R
@@ -8,9 +8,17 @@ input_UI <- function(id) {
           width = NULL,
           collapsible = TRUE,
           status = "primary",
-          "Browse to the location of your input data, which must be formatted according to the template.",
-          br(),
-          tags$a(href="https://github.com/UNHCR-Guatemala/A2SIT/raw/main/inst/data_module-input.xlsx", "For testing purpose, you can download & use this file"),
+
+          "Select the country you want to analyse, then upload your Admin2 data.",
+          "Use the link below to download a template for your selected country, or",
+          tags$a(href="https://github.com/UNHCR-Guatemala/A2SIT/raw/main/inst/data_module-input.xlsx",
+                 "download example data set"),
+          "for testing/demo.",
+          br(),br(),
+
+          country_dropdown(NS(id, "ISO3"), "Select country:"),
+
+          downloadLink(NS(id, "download_country_template"), "Download country template"),
 
           br(),br(),
           fileInput(NS(id, "xlsx_file"), "Load Data", buttonLabel = "Browse...", accept = c("xls", "xlsx")),
@@ -37,6 +45,17 @@ input_server <- function(id, coin, coin_full) {
 
   moduleServer(id, function(input, output, session) {
 
+    # Download template
+    output$download_country_template <- downloadHandler(
+      filename = function() {
+        paste0("A2SIT_data_input_template_", input$ISO3, ".xlsx")
+      },
+      content = function(file) {
+        f_generate_input_template(input$ISO3, file)
+      }
+    )
+
+    # load data
     observeEvent(input$load_click, {
 
       req(input$xlsx_file)
