11d4cb11e29e1eee5129952c964fa63809c9e856
bluefoxr <william.becker@bluefoxdata.eu>
2023-05-18 17:01:08 +0200

    slight change of colour

--- R/theme.R
+++ R/theme.R
@@ -6,7 +6,7 @@ theme_UNHCR <- fresh::create_theme(
   ),
   fresh::adminlte_sidebar(
     width = "300px",
-    #dark_bg = "#DCE9FF",
+    dark_bg = "#0063a3",
     #dark_hover_bg = "#81A1C1",
     #dark_color = "#2E3440"
   ),
