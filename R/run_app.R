6133562667f4a2b0ba54be2d045742e6357ec1d3
bluefoxr <william.becker@bluefoxdata.eu>
2023-07-11 12:20:03 +0200

    check if phantomjs installed and install if not, or message

--- R/run_app.R
+++ R/run_app.R
@@ -6,6 +6,15 @@
 #' @importFrom golem with_golem_options
 #' @export
 run_app <- function(...) {
+  # check if phantomJS installed
+  if(!webshot::is_phantomjs_installed()){
+    webshot::install_phantomjs() |> suppressMessages()
+    # check if succeeded
+    if(!webshot::is_phantomjs_installed()){
+      message("Cannot install phantomJS for some reason. This means that map downloads will be disabled.")
+    }
+  }
+
   with_golem_options(
     app = shinyApp(ui = app_ui, server = app_server, enableBookmarking = "server"),
     golem_opts = list(...)
