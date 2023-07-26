32099caaa03f9b43041acba9a631f0fac63d5535
bluefoxr <william.becker@bluefoxdata.eu>
2023-07-26 11:40:20 +0200

    sort indicators, better code annotation

--- R/mod_analysis.R
+++ R/mod_analysis.R
@@ -2,94 +2,103 @@ analysis_UI <- function(id) {
 
   shinydashboard::tabItem(
     tabName = "analyse",
-    column(7,
+    column(
+      7,
 
-           shinydashboardPlus::box(
-             title = box_pop_title(
-               title = "Indicator analysis",
-               popover_text = "The table shows details about each indicator, with possible issues highlighted in yellow. Please click the main help icon in the upper right for more information.",
-               placement = "bottom", px_from_right = 40
-             ),
-             collapsible = FALSE, width = 12,
-             status = "info",
-             sidebar = shinydashboardPlus::boxSidebar(
-               id = "analysis_table_sidebar",
-               icon = icon("gear"),
-               width = 25,
-               checkboxInput(
-                 NS(id, "filter_table"),
-                 label = "Filter to flagged indicators",
-                 value = FALSE)
-             ),
-             DT::DTOutput(NS(id, "analysis_table"))
-           ),
+      # TABLE BOX
+      shinydashboardPlus::box(
+        title = box_pop_title(
+          title = "Indicator analysis",
+          popover_text = "The table shows details about each indicator, with possible issues highlighted in yellow. Please click the main help icon in the upper right for more information.",
+          placement = "bottom", px_from_right = 40
+        ),
+        collapsible = FALSE, width = 12,
+        status = "info",
+        sidebar = shinydashboardPlus::boxSidebar(
+          id = "analysis_table_sidebar",
+          icon = icon("gear"),
+          width = 25,
+          checkboxInput(
+            NS(id, "filter_table"),
+            label = "Filter to flagged indicators",
+            value = FALSE)
+        ),
+        DT::DTOutput(NS(id, "analysis_table"))
+      ),
 
-           shinydashboardPlus::box(
-             id = NS(id, "indbox"),
-             title = box_pop_title(
-               "Indicator information",
-               popover_text = "Info about the selected indicator. Optionally use the 'Remove' button to remove the indicator, and the 'Restore' button to restore it.",
-               placement = "top", px_from_right = 70),
-             collapsible = TRUE, width = 12, closable = TRUE,
-             status = "info",
-               column(
-                 7,
-                 htmlOutput(NS(id, "indicator_summary"))
-               ),
-               column(
-                 4,
-                 shinyWidgets::actionBttn(
-                   inputId = NS(id, "remove_indicator"),
-                   label = "Remove",
-                   style = "jelly",
-                   color = "danger", icon = icon("minus"), size = "sm"
-                 ),
-                 shinyWidgets::actionBttn(
-                   inputId = NS(id, "add_indicator"),
-                   label = "Restore",
-                   style = "jelly",
-                   color = "success", icon = icon("plus"), size = "sm"
-                 )
-               )
+      # INDICATOR INFO BOX
+      shinydashboardPlus::box(
+        id = NS(id, "indbox"),
+        title = box_pop_title(
+          "Indicator information",
+          popover_text = "Info about the selected indicator. Optionally use the 'Remove' button to remove the indicator, and the 'Restore' button to restore it.",
+          placement = "top", px_from_right = 70),
+        collapsible = TRUE, width = 12, closable = TRUE,
+        status = "info",
+        column(
+          7,
+          htmlOutput(NS(id, "indicator_summary"))
+        ),
+        column(
+          4,
+          shinyWidgets::actionBttn(
+            inputId = NS(id, "remove_indicator"),
+            label = "Remove",
+            style = "jelly",
+            color = "danger", icon = icon("minus"), size = "sm"
+          ),
+          shinyWidgets::actionBttn(
+            inputId = NS(id, "add_indicator"),
+            label = "Restore",
+            style = "jelly",
+            color = "success", icon = icon("plus"), size = "sm"
+          )
+        )
 
-           )
+      )
     ),
-    column(5,
 
-           shinydashboardPlus::box(
-             title = box_pop_title(
-               "Distribution",
-               popover_text = "Distribution plot of the indicator selected in the analysis table. Click the 'gear' icon to the right to change the plot type.",
-               placement = "bottom", px_from_right = 70),
-             collapsible = TRUE,
-             status = "info", width = 12,
-             sidebar = shinydashboardPlus::boxSidebar(
-               id = "violin_sidebar",
-               icon = icon("gear"),
-               width = 40,
-               selectInput(NS(id, "dist_plottype"), label = "Plot type", choices = c("Violin", "Histogram"))
-             ),
-             plotly::plotlyOutput(NS(id, "violin_plot"))
-           ),
 
-           shinydashboardPlus::box(
-             title = box_pop_title(
-               "Scatter plot",
-               popover_text = "The x axis is the indicator selected in the table. Click on the 'gear' icon to the right to select the indicator to plot on the y axis and to optionally use log axes.",
-               placement = "top", px_from_right = 70),
-             collapsible = TRUE,
-             status = "info", width = 12,
-             sidebar = shinydashboardPlus::boxSidebar(
-               id = "scatter_sidebar",
-               icon = icon("gear"),
-               width = 40,
-               selectInput(NS(id, "scat_v2"), label = "Plot against", choices = c("tmp_999")),
-               shinyWidgets::prettySwitch(NS(id, "scat_trend"), label = "Trendline"),
-               shinyWidgets::prettySwitch(NS(id, "scat_logx"), label = "Log X"),
-               shinyWidgets::prettySwitch(NS(id, "scat_logy"), label = "Log Y")
-             ),
-             plotly::plotlyOutput(NS(id, "scatter_plot"))
-           )
+    column(
+      5,
+
+      # DISTRIBUTION PLOT BOX
+      shinydashboardPlus::box(
+        title = box_pop_title(
+          "Distribution",
+          popover_text = "Distribution plot of the indicator selected in the analysis table. Click the 'gear' icon to the right to change the plot type.",
+          placement = "bottom", px_from_right = 70),
+        collapsible = TRUE,
+        status = "info", width = 12,
+        sidebar = shinydashboardPlus::boxSidebar(
+          id = "violin_sidebar",
+          icon = icon("gear"),
+          width = 40,
+          selectInput(NS(id, "dist_plottype"), label = "Plot type", choices = c("Violin", "Histogram"))
+        ),
+        plotly::plotlyOutput(NS(id, "violin_plot"))
+      ),
+
+      # SCATTER PLOT BOX
+      shinydashboardPlus::box(
+        title = box_pop_title(
+          "Scatter plot",
+          popover_text = "The x axis is the indicator selected in the table. Click on the 'gear' icon to the right to select the indicator to plot on the y axis and to optionally use log axes.",
+          placement = "top", px_from_right = 70),
+        collapsible = TRUE,
+        status = "info", width = 12,
+        sidebar = shinydashboardPlus::boxSidebar(
+          id = "scatter_sidebar",
+          icon = icon("gear"),
+          width = 40,
+          p("Select an indicator - you can also type to search in this box."),
+          selectInput(NS(id, "scat_v2"), label = "Plot against", choices = c("tmp_999")),
+          shinyWidgets::prettySwitch(NS(id, "scat_trend"), label = "Trendline"),
+          shinyWidgets::prettySwitch(NS(id, "scat_logx"), label = "Log X"),
+          shinyWidgets::prettySwitch(NS(id, "scat_logy"), label = "Log Y")
+        ),
+        plotly::plotlyOutput(NS(id, "scatter_plot"))
+      )
     )
   )
 
@@ -130,7 +139,7 @@ analysis_server <- function(id, coin, coin_full, parent_input, r_shared) {
     observe({
       updateSelectInput(
         inputId = "scat_v2",
-        choices = coin_full()$Meta$Lineage[[1]]
+        choices = coin_full()$Meta$Lineage[[1]] |> sort()
       )
     })
 
@@ -245,7 +254,6 @@ analysis_server <- function(id, coin, coin_full, parent_input, r_shared) {
       req(r_shared$isel)
       req(input$scat_v2 != "tmp_999")
 
-      browser
       iCOINr::iplot_scatter(
         coin_full(),
         dsets = "Raw",
