c9113d0bd54f99e15cddf2904e910bd0061a1b9c
Will Becker <william.becker@bluefoxdata.eu>
2023-07-27 17:05:16 +0200

    more unit tests

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -25,6 +25,45 @@ test_that("analysis table", {
   same_check <- max(table(coin$Data$Raw$Refugiados))/sum(!is.na(coin$Data$Raw$Refugiados))
   expect_equal(same_Refugiados, same_check)
 
+  # skew/kurt - have to get back to numeric format
+  sk_Afrodesc <- df_fs$SkewKurt[df_fs$iCode == "Afrodesc"] |>
+    strsplit(" / ") |>
+    unlist() |>
+    as.numeric()
 
+  expect_equal(sk_Afrodesc[1], signif(COINr::skew(coin$Data$Raw$Afrodesc, na.rm = TRUE),3))
+  expect_equal(sk_Afrodesc[2], signif(COINr::kurt(coin$Data$Raw$Afrodesc, na.rm = TRUE),3))
+
+  # check negative correlation
+  # from table, Dormitoria is negatively correlated with VivCollectCalle
+
+  cor_check <- stats::cor(coin$Data$Raw$Dormitoria, coin$Data$Raw$VivCollectCalle,
+                          use = "pairwise.complete.obs", method = "spearman")
+  expect_lt(cor_check, -0.4)
+
+  # all indicators should be in
+  expect_true(all(df_fs$Status == "In"))
+
+  # test removing indicator
+  coin <- f_remove_indicators(coin, "Afrodesc")
+  expect_equal(coin$Analysis$Raw$FlaggedStats$Status[coin$Analysis$Raw$FlaggedStats$iCode == "Afrodesc"], "OUT")
+  # test replace indicator
+  coin <- f_add_indicators(coin, "Afrodesc")
+  expect_equal(coin$Analysis$Raw$FlaggedStats$Status[coin$Analysis$Raw$FlaggedStats$iCode == "Afrodesc"], "In")
+
+  # test flags
+  df_f <- coin$Analysis$Raw$Flags
+  expect_equal(ncol(df_f), ncol(df_fs))
+  expect_equal(nrow(df_f), nrow(df_fs))
+
+  # I'm just going to manually check some things based on visual inspection
+  # recall that TRUE means flagged
+  expect_false(all(df_f$Frc.Avail))
+  expect_true(df_f$Frc.Same[df_f$iCode == "Refugiados"])
+  all_negcorr <- !is.na(df_fs$NegCorr)
+  expect_true(all(df_f$NegCorr[all_negcorr]))
+
+  # don't test stats table as this is direct COINr output and tested there
+  # https://github.com/bluefoxr/COINr/blob/master/tests/testthat/test-stats.R
 
 })
---
+++ tests/testthat/test-results.R
@@ -0,0 +1,88 @@
+test_that("results correct", {
+
+  # we can assume that the core COINr functions are correct since they are already
+  # unit tested: https://github.com/bluefoxr/COINr/tree/master/tests/testthat
+
+  # load example coin, then rebuild
+  coin <- GTM_coin
+  coin <- f_build_index(coin, agg_method = "a_amean", only_aggregate = FALSE)
+
+  # the steps are 1. treat, 2. normalise, 3. aggregate
+
+  # TREATMENT
+  dft <- COINr::get_dset(coin, "Treated", also_get = "none")
+  dfr <- COINr::get_dset(coin, "Raw", also_get = "none")
+
+  # manually treat data frame with set paras
+  dft2 <- COINr::qTreat(dfr,
+                        winmax = 5,
+                        skew_thresh = 2,
+                        kurt_thresh = 3.5,
+                        f2 = "log_CT_plus")$x_treat
+  # check same
+  expect_equal(dft, dft2, ignore_attr = TRUE)
+
+  # NORMALISATION
+  dfn <- COINr::get_dset(coin, "Normalised", also_get = "none")
+  # manually normalise
+  dfn2 <- COINr::Normalise(
+    dft, directions = coin$Meta$Ind[c("Direction", "iCode")],
+    global_specs = list(f_n = "n_minmax",
+                        f_n_para = list(l_u = c(1, 100)))
+  )
+  expect_equal(dfn, dfn2, ignore_attr = TRUE)
+
+  # aggregation
+  dfa <- COINr::get_dset(coin, "Aggregated")
+  # some basic checks - take one row
+  x <- dfa[1,]
+  # arithmetic mean calculation
+  expect_equal(x$MVI, mean(c(x$Amenazas, x$Cap_Resp, x$Movilidad, x$Vulnerabilidad)))
+
+  # reaggregate with geometric
+  coin <- f_build_index(coin, agg_method = "a_gmean", only_aggregate = TRUE)
+  dfa <- COINr::get_dset(coin, "Aggregated")
+  x <- dfa[1,]
+  expect_equal(x$MVI, COINr::a_gmean(c(x$Amenazas, x$Cap_Resp, x$Movilidad, x$Vulnerabilidad)))
+
+  # reaggregate with harmonic
+  coin <- f_build_index(coin, agg_method = "a_hmean", only_aggregate = TRUE)
+  dfa <- COINr::get_dset(coin, "Aggregated")
+  x <- dfa[1,]
+  expect_equal(x$MVI, COINr::a_hmean(c(x$Amenazas, x$Cap_Resp, x$Movilidad, x$Vulnerabilidad)))
+
+  # change weights - check
+  coin$Log$new_coin$iMeta$Weight[coin$Log$new_coin$iMeta$iCode == "Amenazas"] <- 0.5
+  coin <- COINr::Regen(coin)
+  dfa <- COINr::get_dset(coin, "Aggregated")
+  x <- dfa[1,]
+  expect_equal(x$MVI, COINr::a_hmean(c(x$Amenazas, x$Cap_Resp, x$Movilidad, x$Vulnerabilidad), w = c(0.5,1,1,1)))
+
+  # SEVERITY categories
+  # go back to default coin
+  coin <- f_build_index(coin, agg_method = "a_amean", only_aggregate = TRUE)
+
+  expect_true("Severity" %in% names(coin$Data))
+  expect_true("Severity" %in% names(coin$Results))
+  sev <- coin$Data$Severity
+
+  # note severity is only applied at the aggregate level
+  # check index level
+  sev_index <- sev$MVI
+
+  # recalculate - get score and then transform
+  MVI <- coin$Data$Aggregated$MVI
+  sev_index2 <- cut(MVI, 5, labels = FALSE)
+  expect_equal(sev_index, sev_index2)
+  # check max score is 5
+  expect_equal(sev_index[MVI == max(MVI, na.rm = TRUE)], 5)
+  # min score is 1
+  expect_equal(sev_index[MVI == min(MVI, na.rm = TRUE)], 1)
+
+})
+
+# also check
+# - add/remove indicators
+# - severity data set is correctly recalculated
+# - results table is correctly reloaded
+# - make sure weights are correctly accounted for when changed in iMeta
