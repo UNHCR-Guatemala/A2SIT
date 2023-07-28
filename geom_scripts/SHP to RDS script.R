0427e23dcfc05b5591725fa4f2072fff2a661069
Pedro Martínez <martinep@unhcr.org>
2023-07-28 10:34:29 -0600

    Adding geometry simplification and .rds generation script

---
+++ geom_scripts/SHP to RDS script.R
@@ -0,0 +1,6 @@
+# SIMPLIFY GEOMETRIES AND CREATE RDS FILES
+## This script allows to simplify .shp files and create RDS files
+## Resulting RDS files should be stored in inst>geom folder for A2SIT to read them
+
+
+
